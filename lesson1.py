4024e6dc8ef528b67b7e8026f425a4ab0fead62e
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-06-07 09:54:24 +0300

    first commit

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+print("Hello Python World!")
+print("This is my first Python program.")
+
+print("I", end="-")
+print("am", end="-")
+print("fine!")
+
+# Escape sequences:
+# \n -> new line
+# \t -> tab
+# \\ -> backslash
+# \' -> single quote
+# \" -> double quote
+print('Hello\ndear\n\t"student"!')
+print("--------------------")
+print("+" * 20)
+
+print("Math expressions: 2 + 3 * 4")
+print(f"Math expressions: {2 + 3 * 4}...{2 + 2}")
